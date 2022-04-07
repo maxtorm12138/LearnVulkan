6ecd93d0f9270aac7c359ada7b302b36890cec2a
maxzwliao <maxzwliao@tencent.com>
2022-04-07 19:23:00 +0800

    xxx

---
+++ include/device_configurator.hpp
@@ -0,0 +1,12 @@
+#pragma once
+// boost
+#include <boost/noncopyable.hpp>
+
+// vulkan
+#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_raii.hpp>
+#include <vulkan/vulkan_structs.hpp>
+namespace lvk
+{
+  class DeviceConfigurator : public boost::noncopyable
+}
