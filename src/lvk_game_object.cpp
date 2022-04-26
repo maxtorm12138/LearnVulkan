9d6dac56a24c13e79c3555e5409a2fe1da51ed2f
maxtorm <maxtorm12138@gmail.com>
2022-04-26 22:51:08 +0800

    refactor

--- src/lvk_game_object.cpp
+++ src/lvk_game_object.cpp
@@ -9,20 +9,20 @@ GameObject::GameObject(size_t id, std::shared_ptr<lvk::Model> model) :
 
 GameObject::GameObject(GameObject &&other) noexcept :
   id_(other.id_),
-  model_(std::move(other.model_)),
-  transform_(other.transform_)
+  model_(std::move(other.model_))
 {
 }
 
-glm::mat4 GameObject::Transform::ModelMatrix() 
+glm::mat4 GameObject::ModelMatrix() const
 {
-    auto model = glm::translate(glm::mat4{1.f}, translation);
-    model = glm::rotate(model, rotation.y, glm::vec3{0, 1, 0});
-    model = glm::rotate(model, rotation.x, glm::vec3{1, 0, 0});
-    model = glm::rotate(model, rotation.z, glm::vec3{0, 0, 1});
-    model = glm::scale(model, scale);
+    auto model = glm::translate(glm::mat4{1.f}, translation_);
+    model = glm::rotate(model, rotation_.y, glm::vec3{0, 1, 0});
+    model = glm::rotate(model, rotation_.x, glm::vec3{1, 0, 0});
+    model = glm::rotate(model, rotation_.z, glm::vec3{0, 0, 1});
+    model = glm::scale(model, scale_);
     return model;
 }
+
 GameObject MakeGameObject(std::shared_ptr<lvk::Model> model)
 {
   static std::atomic<size_t> id{0};
--- src/lvk_render_system.cpp
+++ src/lvk_render_system.cpp
@@ -1,41 +1,52 @@
 #include "lvk_render_system.hpp"
+
+// module
+#include "lvk_hardware.hpp"
+#include "lvk_shader.hpp"
+
+// glm
 #include <glm/ext.hpp>
 
 namespace lvk
 {
 
-RenderSystem::RenderSystem(const lvk::Device &device, const vk::raii::RenderPass &render_pass) :
-    device_(device),
-    pipeline_layout_(ConstructPipelineLayout()),
-    pipeline_(
-        device_,
-        pipeline_layout_,
-        lvk::Shader(device_, "naive","shaders/naive/naive.vert.spv", vk::ShaderStageFlagBits::eVertex),
-        lvk::Shader(device_, "naive","shaders/naive/naive.frag.spv", vk::ShaderStageFlagBits::eFragment),
-        render_pass)
+RenderSystem::RenderSystem(const lvk::Hardware &hardware, const vk::raii::RenderPass &render_pass) :
+    pipeline_layout_(ConstructPipelineLayout(hardware)),
+    pipeline_(hardware, pipeline_layout_, LoadShaders(hardware), render_pass)
 {}
+
+std::vector<lvk::Shader> RenderSystem::LoadShaders(const lvk::Hardware &hardware)
+{
+    std::vector<lvk::Shader> shaders;
+    shaders.emplace_back(hardware, "naive", "shaders/naive/naive.vert.spv", vk::ShaderStageFlagBits::eVertex);
+    shaders.emplace_back(hardware, "naive", "shaders/naive/naive.frag.spv", vk::ShaderStageFlagBits::eFragment);
+    return std::move(shaders);
+}
 
 void RenderSystem::RenderObjects(const vk::raii::CommandBuffer &command_buffer, std::vector<lvk::GameObject> &objects)
 {
     pipeline_.BindPipeline(command_buffer);
     for (auto &object : objects) {
-        auto &transform = object.GetTransform();
-        transform.scale = {0.5f, 0.5f, 0.5f};
-        transform.rotation.y = glm::mod(transform.rotation.y + glm::radians(-0.1f), glm::two_pi<float>());
-        transform.rotation.x = glm::mod(transform.rotation.x + glm::radians(-0.1f), glm::two_pi<float>());
+        object.SetScale({0.5f, 0.5f, 0.5f});
+        auto rotation = object.GetRotation();
+        rotation.y = glm::mod(rotation.y + glm::radians(-0.1f), glm::two_pi<float>());
+        rotation.x = glm::mod(rotation.x + glm::radians(-0.1f), glm::two_pi<float>());
+        object.SetRotation(rotation);
+
         MVP mvp
         {
-            .model = object.GetTransform().ModelMatrix(),
+            .model = object.ModelMatrix(),
             .view = glm::mat4(1.f),
             .projection = glm::mat4(1.f)
         };
+
         command_buffer.pushConstants<MVP>(*pipeline_layout_, vk::ShaderStageFlagBits::eVertex, 0, mvp);
         object.GetModel()->BindBuffer(command_buffer);
         object.GetModel()->Draw(command_buffer);
     }
 }
 
-vk::raii::PipelineLayout RenderSystem::ConstructPipelineLayout()
+vk::raii::PipelineLayout RenderSystem::ConstructPipelineLayout(const lvk::Hardware &hardware)
 {
     vk::PushConstantRange push_constant_range
     {
@@ -50,7 +61,7 @@ vk::raii::PipelineLayout RenderSystem::ConstructPipelineLayout()
         .pPushConstantRanges = &push_constant_range
     };
 
-    return vk::raii::PipelineLayout(device_.get().GetDevice(), pipeline_layout_create_info);
+    return vk::raii::PipelineLayout(hardware.GetDevice(), pipeline_layout_create_info);
 }
 
 }
