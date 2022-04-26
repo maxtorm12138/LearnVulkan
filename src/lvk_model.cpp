fed098a54fbacf38ae9e0c0f09d3c7258eed41fd
maxtorm <maxtorm12138@gmail.com>
2022-04-26 23:29:16 +0800

    refactor

--- src/lvk_model.cpp
+++ src/lvk_model.cpp
@@ -8,8 +8,9 @@ namespace lvk
 {
 
 Model Model::FromVertex(
-    const lvk::Device &device,
+    const lvk::Hardware &hardware,
     const lvk::Allocator& allocator,
+    const vk::raii::CommandPool &command_pool,
     const std::vector<Vertex> &vertices)
 {
     auto size = sizeof(Vertex) * vertices.size();
@@ -26,14 +27,15 @@ Model Model::FromVertex(
     auto data = stage_buffer.MapMemory();
     memcpy(data, vertices.data(), size);
     stage_buffer.UnmapMemory();
-    CopyBuffer(device, stage_buffer, buffer, size);
+    CopyBuffer(hardware, command_pool, stage_buffer, buffer, size);
 
     return Model(vertices.size(), size, 0, 0, std::move(buffer));
 }
 
 Model Model::FromIndex(
-    const lvk::Device &device,
+    const lvk::Hardware &hardware,
     const lvk::Allocator& allocator,
+    const vk::raii::CommandPool &command_pool,
     const std::vector<Vertex> &vertices,
     const std::vector<uint32_t> &indices)
 {
@@ -54,17 +56,24 @@ Model Model::FromIndex(
     data += vertices_size;
     memcpy(data, indices.data(), indices_size);
     stage_buffer.UnmapMemory();
-    CopyBuffer(device, stage_buffer, buffer, vertices_size + indices_size);
+    CopyBuffer(hardware, command_pool, stage_buffer, buffer, vertices_size + indices_size);
     return Model(vertices.size(), vertices_size, indices.size(), indices_size, std::move(buffer));
 }
 
 void Model::CopyBuffer(
-    const lvk::Device &device,
+    const lvk::Hardware &hardware,
+    const vk::raii::CommandPool &command_pool,
     const lvk::Buffer &src_buffer,
     const lvk::Buffer & dest_buffer,
     uint64_t size)
 {
-    auto command_buffers = device.AllocateCopyCommandBuffers(1);
+    auto command_buffers = hardware.GetDevice().allocateCommandBuffers(vk::CommandBufferAllocateInfo
+    {
+        .commandPool = *command_pool,
+        .level = vk::CommandBufferLevel::ePrimary,
+        .commandBufferCount = 1,
+    });
+
     auto &command_buffer = command_buffers[0];
     command_buffer.begin(vk::CommandBufferBeginInfo{.flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
     vk::BufferCopy region
@@ -86,8 +95,8 @@ void Model::CopyBuffer(
     };
     vk::ArrayProxy<const vk::SubmitInfo> submit_infos(submit_info);
 
-    device.GetQueue().submit(submit_infos);
-    device.GetDevice().waitIdle();
+    hardware.GetQueue(Hardware::QueueType::GRAPHICS)->submit(submit_infos);
+    hardware.GetDevice().waitIdle();
 }
 
 Model::Model(uint32_t vertices_count, size_t vertices_size, uint32_t indices_count, size_t indices_size, lvk::Buffer buffer) :
